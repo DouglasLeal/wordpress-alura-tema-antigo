9deaf59bed3702f5c9331dc7ade1a4d8b684fda9
Douglas Leal <douglas.leal@live.com>
2023-03-22 17:04:14 -0300

    Criando novo tipo de post

--- functions.php
+++ functions.php
@@ -1,3 +1,28 @@
 <?php
 
 add_theme_support('post-thumbnails');
+
+function dl_cadastrando_post_type_imoveis(){
+    $labels = [
+        'name' => 'Imóveis',
+        'name_singular' => 'Imóvel',
+        'add_new_item' => "Adicionar novo imóvel",
+        'edit_item' => "Editar Imóvel"
+    ];
+
+    $supports = [
+        'title',
+        'editor',
+        'thumbnail'
+    ];
+
+    $args = [
+        'labels' => $labels,
+        'public' => true,
+        'description' => "Imóveis da imobiliária Malura",
+        'menu_icon' => "dashicons-admin-home",
+        'supports' => $supports
+    ];
+    register_post_type('imovel', $args);
+}
+add_action('init', "dl_cadastrando_post_type_imoveis");
