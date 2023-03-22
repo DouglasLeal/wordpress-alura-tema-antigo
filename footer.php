3eb747c9c3f5759b803a1784e074a0e86e5d5303
Douglas Leal <douglas.leal@live.com>
2023-03-22 13:35:24 -0300

    Criando Header.php e Footer.php. Listando posts no index.php

---
+++ footer.php
@@ -0,0 +1,6 @@
+<hr>
+footer
+
+<?php wp_footer(); ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,13 +1,16 @@
-<!doctype html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello</h1>
-</body>
-</html>
+<?php get_header(); ?>
+<h1>Hello</h1>
+
+<?php
+if (have_posts()):
+    while (have_posts()):
+        the_post();
+?>
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_content(); ?></p>
+<?php
+    endwhile;
+endif;
+?>
+
+<?php get_footer(); ?>
