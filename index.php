dff01ac76692a65ddf30199d4fa57a5ab6d45497
Douglas Leal <douglas.leal@live.com>
2023-03-22 16:22:04 -0300

    trabalhando com thumbnails

--- index.php
+++ index.php
@@ -1,16 +1,27 @@
 <?php get_header(); ?>
-<h1>Hello</h1>
 
-<?php
-if (have_posts()):
-    while (have_posts()):
-        the_post();
-?>
-        <h2><?php the_title(); ?></h2>
-        <p><?php the_content(); ?></p>
-<?php
-    endwhile;
-endif;
-?>
+
+<main class="home-main">
+    <div class="container">
+        <h1>Maluras</h1>
+        <ul class="imoveis-listagem">
+            <?php
+            if (have_posts()):
+                while (have_posts()):
+                    the_post();
+                    ?>
+                    <li class="imoveis-listagem-item">
+                        <h2><?php the_title(); ?></h2>
+                        <?php the_post_thumbnail(); ?>
+                        <p><?php the_content(); ?></p>
+                    </li>
+                <?php
+                endwhile;
+            endif;
+            ?>
+
+        </ul>
+    </div>
+</main>
 
 <?php get_footer(); ?>
