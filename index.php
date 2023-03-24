7ffc796dc640b2e7e03c1285326f0dc36334791f
Douglas Leal <douglas.leal@live.com>
2023-03-24 07:27:20 -0300

    crindo filtro de busca

--- index.php
+++ index.php
@@ -5,9 +5,42 @@
 <main class="home-main">
     <div class="container">
         <h1>Maluras</h1>
+
+        <?php $taxonomies = get_terms('localizacao'); ?>
+        <form class="busca-localizacao-form" action="<?php bloginfo('url'); ?>/" method="get">
+            <div class="taxonomy-select-wrapper">
+                <select name="taxonomy">
+                    <option value="">Todos os imóveis</option>
+                    <?php foreach($taxonomies as $taxonomy) { ?>
+                        <option value="<?= $taxonomy->slug; ?>"><?= $taxonomy->name; ?></option>
+                    <?php } ?>
+                </select>
+            </div>
+            <button type="submit">Filtrar</button>
+        </form>
+
         <ul class="imoveis-listagem">
             <?php
-            $args = ['post_type' => 'imovel'];
+            $queryTaxonomy = array_key_exists('taxonomy', $_GET);
+            if( $queryTaxonomy && $_GET['taxonomy'] == '') {
+                wp_redirect( home_url() );
+                exit;
+            }
+
+            if( $queryTaxonomy ) {
+                $taxonomy_args = [
+                    [
+                        'taxonomy' => 'localizacao',
+                        'field' => 'slug',
+                        'terms' => $_GET['taxonomy']
+                    ]
+                ];
+            }
+
+            $args = [
+                'post_type' => 'imovel',
+                'tax_query' => $taxonomy_args
+            ];
             $loop = new WP_Query($args);
 
             if ($loop->have_posts()):
@@ -16,9 +49,9 @@
                     ?>
                     <li class="imoveis-listagem-item">
                         <a href="<?php the_permalink(); ?>">
-                        <h2><?php the_title(); ?></h2>
-                        <?php the_post_thumbnail(); ?>
-                        <p><?php the_content(); ?></p>
+                            <h2><?php the_title(); ?></h2>
+                            <?php the_post_thumbnail(); ?>
+                            <p><?php the_content(); ?></p>
                         </a>
                     </li>
                 <?php
